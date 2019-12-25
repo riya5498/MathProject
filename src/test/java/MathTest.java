5e2f5eb9d4346f2f096c5d2ce2816517397044ed
riya5498 <50584318+riya5498@users.noreply.github.com>
2019-12-25 17:16:36 +0530

    Update MathTest.java

--- src/test/java/MathTest.java
+++ src/test/java/MathTest.java
@@ -4,32 +4,63 @@ import org.junit.Test;
 
 import com.math.operation.Math;
 
-
 public class MathTest {
 
+	/*
+	 * Test to check if the number is even or not
+	 */
 	@Test
-	public void testEvenodd() {
+	public void testOdd() {
 		Math math = new Math();
 		int result = math.even(13);
-		assertEquals(0,result);
-		}
+		assertEquals(0, result);
+	}
+
+	@Test
+	public void testEven() {
+		Math math = new Math();
+		int result = math.even(12);
+		assertEquals(1, result);
+	}
 
 	@Test
 	public void testFactorial() {
 		Math math = new Math();
 		int result = math.factorial(4);
-		assertEquals(24,result);
-		}
+		assertEquals(24, result);
+	}
+
+	/*
+	 * Test to check if the number is a palindrome or not
+	 */
 	@Test
 	public void testPalindrome() {
 		Math math = new Math();
 		int result = math.palin(161);
-		assertEquals(1,result);
-		}
+		assertEquals(1, result);
+	}
+
+	@Test
+	public void testnoPalindrome() {
+		Math math = new Math();
+		int result = math.palin(123);
+		assertEquals(0, result);
+	}
+
+	/*
+	 * Test to check if the number is prime or not
+	 */
 	@Test
 	public void testPrime() {
 		Math math = new Math();
+		boolean result = math.primeNo(11);
+		assertEquals(true, result);
+	}
+
+	@Test
+	public void testnotPrime() {
+		Math math = new Math();
 		boolean result = math.primeNo(6);
-		assertEquals(false,result);
-	}	
+		assertEquals(false, result);
+	}
 }
